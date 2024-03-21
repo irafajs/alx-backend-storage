5291263317274199766fd13dd0a80342cba755aa
irafajs <irafajs@gmail.com>
2024-03-21 10:37:26 +0200

    TASK03 YEAR CORRECTION

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- CALCULATE THE YEAR THEY PLAYED TOGOTHER
 SELECT band_name,
 CASE
-	WHEN split IS NULL THEN 2020 - formed
+	WHEN split IS NULL THEN 2022 - formed
 	ELSE split - formed
 	END AS lifespan
 FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
