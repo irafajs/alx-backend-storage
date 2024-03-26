dc67ad479dc318de83454c5e92f4721ca7a9d9a2
irafajs <irafajs@gmail.com>
2024-03-26 22:23:37 +0200

    TASK12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Shebang to create a PY script
+"""
+
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Methods to count the logs and the method init"""
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
+    status_check_count = collection.count_documents(
+            {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
