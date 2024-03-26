452e76aaf79511cb3b56b10d282a6fe1ec7b03f2
irafajs <irafajs@gmail.com>
2024-03-26 22:32:51 +0200

    TASK UPDATES 1 CORRECTION

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -18,7 +18,7 @@ def log_stats():
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print(f"\tmethod {method}: {count}")
     status_check_count = collection.count_documents(
             {"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
